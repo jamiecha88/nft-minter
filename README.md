242d188c60f0bd1f051fbd67281bd78b5380e9c1
Jamie Cha <chaeunhee98@gmail.com>
2023-04-06 17:54:01 -0500

    create minter component

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 🗃 NFT Minter
+
+## About
+
+An NFT minter using Metamask and Web3. Connect a smart contract to your React dApp.
+
+## 🤔 Note
+
+Before running `npm start` in your terminal to start the React project, you need to run `npm install` to download all the package.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Minter from './Minter'
+import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react'
 
 function App() {
   return (
-    <div className="App">
-      <Minter></Minter>
-    </div>
+    <ThirdwebProvider>
+      <div className="App">
+        <Minter></Minter>
+      </div>
+    </ThirdwebProvider>
   );
 }
 
